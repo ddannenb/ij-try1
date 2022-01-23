43a6c5fda8f71b8757f29110aa320b94d6f45268
daved <daved@audiblelogic.com>
2022-01-23 10:52:41 -0700

    Initial

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button
+      @click="testBtn"
+      style="width: 80px;
+      height: 40px">Press me</button>
   </div>
 </template>
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "About",
+  components: {},
+  methods: {
+    testBtn() {
+      console.log("testBtn");
+    },
+  },
+});
+</script>
